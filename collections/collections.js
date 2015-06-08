14d258848e809a4b554a313dd60f63be02805470
Wilfred Wee <weezhizhiang@hotmail.com>
2015-06-08 10:12:13 -0700

    Remove use strict when we declare our globals.
    Obviously not the best solution, but it is the most practical one for what
    we need in Meteor right now.

--- collections/collections.js
+++ collections/collections.js
@@ -1,4 +1,8 @@
-"use strict";
+// For now, we don't use strict when we declare our global variables.
+// The workaround for now is more work than necessary, and Meteor is
+// currently very reliant on global already anyway.
+// TODO: Set Tournaments into the DeclashApp namespace.
+
 /* global Tournaments:true */
 
 Tournaments = new Mongo.Collection("tournaments");
